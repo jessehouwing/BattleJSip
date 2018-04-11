d23b14bfb4fff31891a9e8bf5c2b591bf7f0b155
Frank van Wijk <fvanwijk@xebia.com>
2018-04-11 16:44:27 +0200

    Initialize board WIP

--- Case Studies/Battleship/Source/Battleship_JS/src/index.js
+++ Case Studies/Battleship/Source/Battleship_JS/src/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import { initializeBoard, initializeEnemyBoard, placeShip } from './game/board-service';
 
-ReactDOM.render(<App selected={console.log.bind(console)} />, document.getElementById('app'));
+// Create enemy board (ships are already placed)
+const enemyBoard = initializeEnemyBoard();
+
+// Place my ships
+const myBoard = initializeBoard();
+
+myBoard.fleet.forEach(ship => {
+  let currentPosition;
+
+  function placeMyShip(param) {
+    if (currentPosition) {
+      currentPosition = param;
+    } else {
+      placeShip(myBoard, 0, currentPosition, param);
+      currentPosition = undefined;
+    }
+  }
+
+  const initializeGame = <App text={!!currentPosition ? `Select position for ${ship.name}` : 'Select direction for ship 1'}
+       selected={placeMyShip}
+       selectDirection={!!currentPosition}/>;
+
+  ReactDOM.render(initializeGame, document.getElementById('app'));
+});
+
+function shoot(position) {
+  console.log(`Shoot at position ${position}`);
+}
+
+ReactDOM.render(<App text="Play game" selected={shoot} />, document.getElementById('app'));
