c69db188c72d6a54567af47e32f873d770fa46f6
None <None>
2018-04-11 16:07:41 +0200

    Add a board UI

--- Case Studies/Battleship/Source/Battleship_JS/src/App.jsx
+++ Case Studies/Battleship/Source/Battleship_JS/src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-export default function App() {
+function getSequence(length) {
+  return Array.from({length}).fill(0).map((e, i) => i);
+}
+
+function getLetter(i) {
+  return String.fromCharCode('A'.charCodeAt(0) + i);
+}
+
+const boardSize = 8;
+
+export default function App({selectDirection, selected}) {
   return (
-    <div>App</div>
+    <table>
+      <thead>
+      <th></th>
+      {getSequence(boardSize).map(i => <th>{getLetter(i)}</th>)}
+      </thead>
+      <tbody>
+      {getSequence(boardSize).map((i) => <tr><td><strong>{i}</strong></td>{
+        getSequence(boardSize).map((j) => <td><button onClick={() => selected(getLetter(j) + i)}>{getLetter(j) + i}</button></td>)
+      }</tr>)}
+      </tbody>
+    </table>
   );
 }
