fadd09c6afd78c08e22ab474c9136ada63a3f6a7
None <None>
2018-04-11 16:41:44 +0200

    Add direction selector and header

--- Case Studies/Battleship/Source/Battleship_JS/src/App.jsx
+++ Case Studies/Battleship/Source/Battleship_JS/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 function getSequence(length) {
   return Array.from({length}).fill(0).map((e, i) => i);
@@ -10,20 +10,60 @@ function getLetter(i) {
 
 const boardSize = 8;
 
-export default function App({selectDirection, selected}) {
+const Board = ({selected}) => {
   return (
     <table>
       <thead>
       <tr>
-      <th></th>
-      {getSequence(boardSize).map(i => <th>{getLetter(i)}</th>)}
+        <th></th>
+        {getSequence(boardSize).map(i => <th>{getLetter(i)}</th>)}
       </tr>
       </thead>
       <tbody>
-      {getSequence(boardSize).map((i) => <tr><td><strong>{i}</strong></td>{
-        getSequence(boardSize).map((j) => <td><button onClick={() => selected(getLetter(j) + i)}>{getLetter(j) + i}</button></td>)
-      }</tr>)}
+      {getSequence(boardSize).map((i) => <tr>
+        <td><strong>{i}</strong></td>
+        {
+          getSequence(boardSize).map((j) => <td>
+            <button onClick={() => selected(getLetter(j) + i)}>{getLetter(j) + i}</button>
+          </td>)
+        }</tr>)}
       </tbody>
     </table>
   );
+}
+
+const DirectionSelector = ({selected}) => (
+  <table>
+    <tbody>
+    <tr>
+      <td></td>
+      <td>
+        <button onClick={() => selected('up')}>▲</button>
+      </td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>
+        <button onClick={() => selected('left')}>◀</button>
+      </td>
+      <td></td>
+      <td>
+        <button onClick={() => selected('right')}>▶</button>
+      </td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>
+        <button onClick={() => selected('down')}>▼</button>
+      </td>
+      <td></td>
+    </tr>
+    </tbody>
+  </table>);
+
+export default function App({text, selectDirection, selected}) {
+  return <Fragment>
+    <h1>{text}</h1>
+    {selectDirection ? <DirectionSelector {...{selected}}/> : <Board {...{selected}} />}
+  </Fragment>
 }
