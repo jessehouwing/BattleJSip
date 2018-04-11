4ad809110812f8409ff12b493c7623ef52d31ac1
Frank van Wijk <fvanwijk@xebia.com>
2018-04-11 14:16:35 +0200

    Render hello world

--- Case Studies/Battleship/Source/Battleship_JS/src/index.js
+++ Case Studies/Battleship/Source/Battleship_JS/src/index.js
@@ -1,4 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
 import './app.css';
 
-console.log('Hello');
-
+ReactDOM.render(<App />, document.getElementById('app'));
