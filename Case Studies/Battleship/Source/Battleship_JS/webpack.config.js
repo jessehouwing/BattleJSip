f55aea51166a4eecde142926061d5ce93e8c104e
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-05-03 08:37:54 +0200

    add production build

--- Case Studies/Battleship/Source/Battleship_JS/webpack.config.js
+++ Case Studies/Battleship/Source/Battleship_JS/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index',
   mode: 'development',
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].[chunkhash].js',
+    path: path.resolve('dist')
   },
 
   module: {
@@ -20,5 +22,11 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
 };
